e3e7372a30a493614e158c475c10508db538b4cd
Benjamin Wohlwend <piquadrat@gmail.com>
2013-08-07 18:00:02 +0200

    use https for youtube

--- djangocms_oembed/oembed_providers.py
+++ djangocms_oembed/oembed_providers.py
@@ -5,6 +5,9 @@ from micawber import ProviderRegistry, Provider, bootstrap_basic
 def bootstrap(cache=None):
     # micawbers own bootstrap basic plus some more
     pr = bootstrap_basic(cache=cache)
-    # add https support for vimeo
+    # add https support for vimeo and youtube
     pr.register('https://vimeo.com/\S*', Provider('https://vimeo.com/api/oembed.json'))
+    pr.unregister('https?://(\S*.)?youtu(\.be/|be\.com/watch)\S+')
+    pr.register('https?://(\S*.)?youtu(\.be/|be\.com/watch)\S+', Provider('https://www.youtube.com/oembed?scheme=https'))
     return pr
+
